195aefea0facca8d2f89d0e1984861333a016839
Chonwai <chonwaiun@gmail.com>
2023-03-21 12:28:09 +0800

    Update: Updated the recognize table function.

--- routes/api/form_recognize.py
+++ routes/api/form_recognize.py
@@ -14,9 +14,7 @@ model_id = os.getenv("AZURE_ABSENCE_FORM_MODEL_ID")
 def form_recognize_absence():
     try:
         form_url = request.form.get('document_url')
-        print(form_url)
         res = AzureFormService.analysisForm(model_id, form_url)
-        print(res)
         absenceFormData = FormService.mapAbsenceForm(res)
         return jsonify({'status': True, 'form_url': form_url, 'absence_form_data': absenceFormData, 'form': res})
     except Exception as e:
@@ -28,11 +26,8 @@ def form_recognize_absence():
 def form_recognize_specific():
     try:
         form_url = request.form.get('document_url')
-        print(form_url)
         model_id = request.form.get('model_id')
-        print(model_id)
         res = AzureFormService.analysisForm(model_id, form_url)
-        print(res)
         recognizeFormData = FormService.mapForm(res, model_id)
         return jsonify({'status': True, 'form_url': form_url, 'recognized_form_data': recognizeFormData, 'form': res})
     except Exception as e:
