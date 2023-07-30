58bc4792e85a2424836e9fb78ea7b6267ad31802
Chonwai <chonwaiun@gmail.com>
2023-07-31 00:01:32 +0800

    Add: Added the try catch when saving the model.

--- services/classification.py
+++ services/classification.py
@@ -80,7 +80,10 @@ class ClassificationService:
             raise ValueError("label must be a string or a list")
         print(numpy.array([label]))
         learner.teach(embeddings.reshape(1, -1), numpy.array([label]))
-        with open('./model/model_{schema_name}.pkl'.format(schema_name=model), 'wb') as file:
-            pickle.dump(learner, file)
-            print('Model Saved!')
+        try:
+            with open('./model/model_{schema_name}.pkl'.format(schema_name=model), 'wb') as file:
+                pickle.dump(learner, file)
+                print('Model Saved!')
+        except Exception as e:
+            print(e)
         return True
