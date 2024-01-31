b842c12ba6ae8471bf89ef9138d4873edb1f37f9
Chonwai <chonwaiun@gmail.com>
2024-01-31 18:22:07 +0800

    Add: Added the query to storybook API.

--- routes/api/generate.py
+++ routes/api/generate.py
@@ -1,5 +1,11 @@
-from flask import Blueprint, jsonify, request
+import os
+from dotenv import load_dotenv
+from flask import Blueprint, jsonify, request, send_file
 from services.generate import GenerateService
+from utils.storybook_generate import BuildBook
+from utils.pdf_generate import build_pdf
+
+load_dotenv()
 
 generate = Blueprint("generate", __name__)
 
@@ -56,5 +62,22 @@ def generate_statistics():
             return jsonify({"status": True, "result": result, "sql": sql})
         else:
             return jsonify({"status": False, "result": result, "sql": sql})
+    except Exception as e:
+        return jsonify({"status": False, "message": "Error: " + str(e)})
+
+@generate.route("/generate/storybook", methods=["POST"])
+def generate_storybook():
+    try:
+        requestData = request.get_json()
+        query = requestData["query"]
+        style = requestData["style"]
+        print(query, style)
+        build_book = BuildBook(os.getenv("OPENAI_GPT4_MODEL_NAME"), query, style)
+        pages = build_book.list_of_tuples
+        finished_pdf = build_pdf(pages, 'result.pdf')
+        file_bytes = open(finished_pdf, 'rb').read()
+        
+        # return file_bytes
+        return send_file(finished_pdf, as_attachment=True, download_name="storybook.pdf")
     except Exception as e:
         return jsonify({"status": False, "message": "Error: " + str(e)})
