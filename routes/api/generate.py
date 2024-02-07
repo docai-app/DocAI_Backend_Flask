4e8a88828e7bea317772f89e79dcb60a0c3659ef
Chonwai <chonwaiun@gmail.com>
2024-02-07 17:16:29 +0800

    Update: Updated the generated style.

--- routes/api/generate.py
+++ routes/api/generate.py
@@ -4,11 +4,20 @@ from flask import Blueprint, jsonify, request, send_file
 from services.generate import GenerateService
 from utils.storybook_generate import BuildBook
 from utils.pdf_generate import build_pdf
+from services.storage import StorageService
 import uuid
 
 load_dotenv()
 
 generate = Blueprint("generate", __name__)
+
+STYLES = {
+    "Impressionism": "Monet, impressionist art style, loose brushstrokes, vibrant colors, painted, painted light",
+    "Ghibli": "Ghibli art style, Studio Ghibli, Hayao Miyazaki, anime, colorful, animated, Japanese culture",
+    "Japanese Ukiyo-e": "Ukiyo-e art style, Hokusai, woodblock prints, flat areas of color, outlines, nature, Japanese culture",
+    "Folk Art": "Folk art style, naive art, simple shapes, bright colors, childlike, intuitive, traditional",
+    "Expressionism": "Expressionist art style, Edvard Munch, distorted forms, dramatic colors, emotional impact, subjective",
+}
 
 
 @generate.route("/generate/chart", methods=["POST"])
@@ -75,9 +84,13 @@ def generate_storybook():
         query = requestData["query"]
         style = requestData["style"]
         print(query, style)
-        buildBook = BuildBook(os.getenv("OPENAI_GPT4_MODEL_NAME"), query, style)
+        buildBook = BuildBook(
+            os.getenv("OPENAI_GPT4_MODEL_NAME"), query, f"{STYLES[style]}"
+        )
         pages = buildBook.list_of_tuples
-        finishedPdf = build_pdf(pages, "result/storybook/storybook_" + str(bookId) + ".pdf")
+        finishedPdf = build_pdf(
+            pages, "./result/storybook/storybook_" + str(bookId) + ".pdf"
+        )
 
         # return file_bytes
         return send_file(finishedPdf, as_attachment=True, download_name="storybook.pdf")
